78e5d90811be3bfb7a07fbd724339daccb124b84
Alexander Boutin Flegel <alexander.boutinFlegel@propellerhead.co.nz>
2020-03-09 09:42:29 +1300

    Added the HTML5 Table registration and compliance event

---
+++ Dotnet.Formview.ControlUtilities/GridViewEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web.UI.WebControls;
+
+namespace Dotnet.Formview.ControlUtilities
+{
+	public static class GridViewEventHandler
+	{
+		public static void Html5PreRenderEvent(object sender, EventArgs e)
+		{
+			//each section must be done in order of thead -> tbody -> tfooter
+
+			//both header sections must be done together
+			try { ((GridView)sender).HeaderRow.TableSection = TableRowSection.TableHeader; } catch { }
+			try { ((GridView)sender).TopPagerRow.TableSection = TableRowSection.TableHeader; } catch { }
+
+			//all body items must be reset
+			foreach (GridViewRow row in ((GridView)sender).Rows)
+			{
+				try { row.TableSection = TableRowSection.TableBody; } catch { }
+			}
+
+			//both footer sections must be done together
+			try { ((GridView)sender).FooterRow.TableSection = TableRowSection.TableFooter; } catch { }
+			try { ((GridView)sender).BottomPagerRow.TableSection = TableRowSection.TableFooter; } catch { }
+		}
+	}
+}
---
+++ Dotnet.Formview.ControlUtilities/GridviewCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Web.UI.WebControls;
+
+namespace Dotnet.Formview.ControlUtilities
+{
+	public static class GridviewCollectionExtensions
+	{
+		/// <summary>
+		/// For every grid view in all page controls process them to properly use thead, tbody, and tfooter
+		/// </summary>
+		public static void MakeGridViewsHtml5(this IEnumerable<GridView> controlSet) => controlSet.RegisterPreRenderToAll(GridViewEventHandler.Html5PreRenderEvent);
+	}
+}

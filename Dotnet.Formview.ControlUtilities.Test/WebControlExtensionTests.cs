21ec5401a6719d8ab98f1fb8440d5023b0b21cbd
Alexander Boutin Flegel <alexander.boutinFlegel@propellerhead.co.nz>
2020-03-09 10:11:17 +1300

    Added some styling utilities to add and remove css classes from a WebControl

---
+++ Dotnet.Formview.ControlUtilities.Test/WebControlExtensionTests.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.UI.WebControls;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dotnet.Formview.ControlUtilities.Test
+{
+	[TestClass]
+	public class WebControlExtensionTests
+	{
+		private TextBox BaseControl => new TextBox
+		{
+			CssClass = "class1 class2"
+		};
+
+		private string NullString => null;
+
+		[TestMethod]
+		public void AddingClasses()
+		{
+			var control = BaseControl;
+
+			control.AddCssClass("class3 class4");
+
+			control.CssClass.Should().Be("class1 class2 class3 class4");
+		}
+
+		[TestMethod]
+		public void AddingListClasses()
+		{
+			var control = BaseControl;
+
+			control.AddCssClass(new List<string> { "class3", "class4"  });
+
+			control.CssClass.Should().Be("class1 class2 class3 class4");
+		}
+
+		[TestMethod]
+		public void AddingDuplicateClasses()
+		{
+			var control = BaseControl;
+
+			control.AddCssClass("class2 class3");
+
+			control.CssClass.Should().Be("class1 class2 class3");
+		}
+
+		[TestMethod]
+		public void BaseNullTollerance()
+		{
+			var control = new TextBox()
+			{
+				CssClass = NullString
+			};
+
+			control.CssClass.Should().Be("");
+
+			control.AddCssClass(NullString);
+			control.CssClass.Should().Be("");
+
+			control.AddCssClass("          ");
+			control.CssClass.Should().Be("");
+
+			control.RemoveCssClass(NullString);
+			control.CssClass.Should().Be("");
+		}
+
+		[TestMethod]
+		public void MutableNullTollerance()
+		{
+			var control = BaseControl;
+
+			control.AddCssClass(NullString);
+
+			control.CssClass.Should().Be("class1 class2");
+
+			control.RemoveCssClass(NullString);
+
+			control.CssClass.Should().Be("class1 class2");
+		}
+
+		[TestMethod]
+		public void RemoveClass()
+		{
+			var control = BaseControl;
+
+			control.RemoveCssClass("class2");
+
+			control.CssClass.Should().Be("class1");
+		}
+
+		[TestMethod]
+		public void RemoveListClass()
+		{
+			var control = BaseControl;
+
+			control.RemoveCssClass(new List<string> { "class1", "class2" });
+
+			control.CssClass.Should().Be("");
+		}
+
+		[TestMethod]
+		public void RemoveDuplicateClass()
+		{
+			var control = BaseControl;
+
+			control.RemoveCssClass(new List<string> { "class1", "class1" });
+
+			control.CssClass.Should().Be("class2");
+		}
+
+		[TestMethod]
+		public void RemoveNoDuplicateClass()
+		{
+			var control = BaseControl;
+
+			control.RemoveCssClass("class3");
+
+			control.CssClass.Should().Be("class1 class2");
+		}
+	}
+}
---
+++ Dotnet.Formview.ControlUtilities/WebControlExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.UI.WebControls;
+
+namespace Dotnet.Formview.ControlUtilities
+{
+	public static class WebControlExtensions
+	{
+		/// <summary>
+		/// Creates a list of css classes from a space separated string. Returns an empty list if input is null or whitespace
+		/// </summary>
+		/// <param name="classes"></param>
+		/// <returns></returns>
+		private static List<string> GetList(string classes) => string.IsNullOrWhiteSpace(classes) ? new List<string>() : new List<string>(classes.Split(' '));
+
+		/// <summary>
+		/// Adds a set of classes to the control
+		/// </summary>
+		/// <param name="control"></param>
+		/// <param name="classesToAdd"></param>
+		/// <returns></returns>
+		public static void AddCssClass(this WebControl control, string classesToAdd)
+			=> control.AddCssClass(GetList(classesToAdd));
+
+		/// <summary>
+		/// Adds a set of classes to the control
+		/// </summary>
+		/// <param name="control"></param>
+		/// <param name="classesToAdd"></param>
+		public static void AddCssClass(this WebControl control, List<string> classesToAdd)
+			=> control.CssClass = string.Join(" ", GetList(control.CssClass).Union(classesToAdd));
+
+		/// <summary>
+		/// Removes a set of classes from the control
+		/// </summary>
+		/// <param name="control"></param>
+		/// <param name="classesToRemove"></param>
+		public static void RemoveCssClass(this WebControl control, string classesToRemove) => control.RemoveCssClass(GetList(classesToRemove));
+
+		/// <summary>
+		/// Removes a set of classes from the control
+		/// </summary>
+		/// <param name="control"></param>
+		/// <param name="classesToRemove"></param>
+		public static void RemoveCssClass(this WebControl control, List<string> classesToRemove)
+			=> control.CssClass = string.Join(" ", GetList(control.CssClass).Where(w => !classesToRemove.Contains(w)));
+	}
+}
